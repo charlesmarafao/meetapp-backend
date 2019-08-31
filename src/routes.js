8dadbe9937ab1b80f3c3f70ca1b455ca1e273880
charlesmarafao <charlesmarafao@gmail.com>
2019-08-31 21:31:49 +0000

    feat: add password hash before save user

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Nice' });
-});
+routes.post('/users', UserController.store);
 
 export default routes;
